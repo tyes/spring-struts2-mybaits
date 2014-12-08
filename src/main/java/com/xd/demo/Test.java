a67136e2188fbf335442f1a2c01896182cb80afa
tyes <xd195666916@163.com>
2014-12-08 18:21:02 +0800

    test coding

---
+++ src/main/java/com/xd/demo/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.xd.demo;
+
+/**
+ * @author xiaodong
+ * @date 2014年12月8日 下午6:20:29
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
