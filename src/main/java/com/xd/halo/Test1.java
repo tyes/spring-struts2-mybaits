49a29c7c9b48858ea7ef2334af330d2ec4ed9ac8
tyes <xd195666916@163.com>
2014-12-15 14:45:34 +0800

    test coding

---
+++ src/main/java/com/xd/halo/Test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.xd.halo;
+
+/**
+ * @author ES-BJ-PC-142
+ * @date 2014年12月15日 下午2:44:38
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
